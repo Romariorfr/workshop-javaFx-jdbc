fe0d0cf2529816cdf95b39437b727a9bfd3a190b
Romário Ferreira <romarioferreirarezende@hotmail.com>
2021-10-10 16:44:54 -0300

    About view

--- src/gui/MainViewControler.java
+++ src/gui/MainViewControler.java
@@ -1,41 +1,69 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
-public class MainViewControler implements Initializable{
-	
+public class MainViewControler implements Initializable {
+
 	@FXML
 	private MenuItem menuItemSeller;
-	
+
 	@FXML
 	private MenuItem menuItemDepartament;
-	
+
 	@FXML
 	private MenuItem menuItemAbout;
-	
+
 	@FXML
-	public void onMenuItemSellerAction(){
+	public void onMenuItemSellerAction() {
 		System.out.println("onMenuItemSellerAction");
 	}
+
 	@FXML
-	public void onMenuItemDepartamentAction(){
+	public void onMenuItemDepartamentAction() {
 		System.out.println("onMenuItemDepartamentAction");
 	}
+
 	@FXML
-	public void onMenuItemAboutAction(){
-		System.out.println("onMenuItemAboutAction");
+	public void onMenuItemAboutAction() {
+		loadView("/gui/About.fxml");
 	}
-	
 
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
+
+	}
+	
+	private void loadView(String AbsoluteName) {
 		
+		try {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource(AbsoluteName));
+		VBox newVBox = loader.load();
+		Scene mainScene = Main.getMainScene();
+		VBox mainVbox = (VBox)((ScrollPane)mainScene.getRoot()).getContent();
 		
+		Node mainMenu = mainVbox.getChildren().get(0);
+		mainVbox.getChildren().clear();
+		mainVbox.getChildren().add(mainMenu);
+		mainVbox.getChildren().addAll(newVBox.getChildren());
+		
+		}catch(IOException e){
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+			
+		}
 	}
 
 }
